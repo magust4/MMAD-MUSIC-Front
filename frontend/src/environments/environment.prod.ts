92e695c35bdda4cc54555920e600910664071ec8
magust4 <149904845+magust4@users.noreply.github.com>
2026-07-15 22:16:07 -0500

    update

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  //apiUrl: 'https://mmad-music.onrender.com'
-  apiUrl: 'http://localhost:8080'
+  apiUrl: 'https://mmad-music.onrender.com'
+  //apiUrl: 'http://localhost:8080'
 };
